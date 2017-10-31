2d22746668872cfd9405f610d194d362e39e3fdc
Ricardo Hortelano <r.hortelano@alumnos.upm.es>
2017-10-31 17:45:40 +0100

    Fixed indentaition

--- modules/template.py
+++ modules/template.py
@@ -7,16 +7,16 @@ echo "Hello"
 '''
 
     #Execute the program
-	for proc in bashCommand.split('\n'):
-		#If proc is not empty
-		if proc:
-			print(proc.split())
-			try:
-				process = subprocess.Popen(proc, stdout=subprocess.PIPE, shell=True)
-				output, error = process.communicate()
-			except OSError as e:
-				print(e)
-				break
+    for proc in bashCommand.split('\n'):
+        #If proc is not empty
+        if proc:
+            print(proc.split())
+            try:
+                process = subprocess.Popen(proc, stdout=subprocess.PIPE, shell=True)
+                output, error = process.communicate()
+            except OSError as e:
+                print(e)
+                break
 
 def getDescription():
 	description = "The template of the variables"
